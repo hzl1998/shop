9eb54b721a6417fcc37ae2961b34a6948eb46c35
hzl1998 <1445419939@qq.com>
2020-05-09 14:19:12 +0800

    关闭 basic 认证

--- vue_shop_server/src/main/java/com/hzl/config/WebSecurityConfig.java
+++ vue_shop_server/src/main/java/com/hzl/config/WebSecurityConfig.java
@@ -63,6 +63,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.logout().logoutSuccessHandler(logoutSuccessHandler);
         http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
         http.exceptionHandling().accessDeniedHandler(accessDeniedHandler);
+        http.authorizeRequests().anyRequest().permitAll().and().logout().permitAll();//关闭 basic 认证
     }
 
     @Override
