da5cb80273c35eb7558128171a522c4ad0be49a1
hzl1998 <1445419939@qq.com>
2020-05-09 14:55:01 +0800

    小修改

--- vue_shop/src/components/Home.vue
+++ vue_shop/src/components/Home.vue
@@ -13,19 +13,19 @@
       <!-- 侧边栏 -->
       <el-aside width="200px">
         <!-- 侧边栏菜单区域 -->
-        <el-menu background-color="#333744" text-color="#fff" active-text-color="#ffd04b">
+        <el-menu background-color="#333744" text-color="#fff" active-text-color="#409BFF">
           <!-- 一级菜单 -->
-          <el-submenu index="1">
+          <el-submenu :index="item.id" v-for="item in menuList" :key="item.id">
             <!-- 一级菜单的模板区域 -->
             <template slot="title">
               <i class="el-icon-location"></i>
-              <span>导航一</span>
+              <span>{{item.name}}</span>
             </template>
             <!-- 二级菜单 -->
-            <el-menu-item index="1-4-1">
+            <el-menu-item :index="subItem.id" v-for="subItem in item.children" :key="subItem.id">
               <template slot="title">
-                <i class="el-icon-location"></i>
-                <span>导航一</span>
+                <i class="el-icon-menu"></i>
+                <span>{{subItem.name}}</span>
               </template>
             </el-menu-item>
           </el-submenu>
@@ -39,10 +39,34 @@
 
 <script>
 export default {
+  data() {
+    return {
+      menuList: []
+    };
+  },
+  created() {
+    this.getMenuList();
+  },
   methods: {
     logout() {
       window.sessionStorage.clear();
       this.$router.push("/login");
+    },
+    //获取菜单
+    getMenuList() {
+      this.$http({
+        method: "GET",
+        url: "menus"
+      })
+        .then(resp => {
+          if (resp.data.code !== 200) {
+            return this.$message.error(resp.data.msg);
+          }
+          this.menuList = resp.data.data;
+        })
+        .catch(error => {
+          console.log(error);
+        });
     }
   }
 };
@@ -73,7 +97,7 @@ export default {
 .el-main {
   background-color: #eaedf1;
 }
-.el-menu{
-    border-right-width: 0;
+.el-menu {
+  border-right-width: 0;
 }
 </style>
