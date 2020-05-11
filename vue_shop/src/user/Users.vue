a0d444dccd55e33def3ae73b6eda71f65076c9a2
hzl1998 <1445419939@qq.com>
2020-05-11 16:46:29 +0800

    添加用户

--- vue_shop/src/user/Users.vue
+++ vue_shop/src/user/Users.vue
@@ -12,12 +12,12 @@
       <!-- 搜索与添加区域 -->
       <el-row :gutter="20">
         <el-col :span="8">
-          <el-input placeholder="请输入内容">
-            <el-button slot="append" icon="el-icon-search"></el-button>
+          <el-input placeholder="请输入内容" v-model="username" clearable @clear="searchUserList">
+            <el-button slot="append" icon="el-icon-search" @click="searchUserList"></el-button>
           </el-input>
         </el-col>
         <el-col :span="4">
-          <el-button type="primary">添加用户</el-button>
+          <el-button type="primary" @click="addDialogVisible = true">添加用户</el-button>
         </el-col>
       </el-row>
 
@@ -28,35 +28,60 @@
         <el-table-column label="邮箱" prop="email"></el-table-column>
         <el-table-column label="电话" prop="phone"></el-table-column>
         <el-table-column label="角色" prop="role_name"></el-table-column>
-         <el-table-column label="状态" prop="enabled">
+        <el-table-column label="状态" prop="enabled">
           <template slot-scope="scope">
-            <el-switch v-model="scope.row.enabled"></el-switch>
+            <el-switch v-model="scope.row.enabled" @change="userStateChanged(scope.row)"></el-switch>
           </template>
         </el-table-column>
         <el-table-column label="创建时间" prop="create_time" :formatter="dateForma"></el-table-column>
         <el-table-column label="操作" width="180px">
           <template slot-scope="scope">
-              <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
-              <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
-              
-               <el-tooltip effect="dark" content="分配角色" placement="top" :enterable="false">
-      <el-button type="warning" icon="el-icon-setting" size="mini"></el-button>
-    </el-tooltip>
+            <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
+            <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
+
+            <el-tooltip effect="dark" content="分配角色" placement="top" :enterable="false">
+              <el-button type="warning" icon="el-icon-setting" size="mini"></el-button>
+            </el-tooltip>
           </template>
         </el-table-column>
       </el-table>
 
       <!-- 分页区域 -->
       <el-pagination
-      @size-change="handleSizeChange"
-      @current-change="handleCurrentChange"
-      :current-page="page"
-      :page-sizes="[2, 4, 6, 10]"
-      :page-size="rows"
-      layout="total, sizes, prev, pager, next, jumper"
-      :total="total">
-    </el-pagination>
+        background
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="page"
+        :page-sizes="[2, 4, 6, 10]"
+        :page-size="rows"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+      ></el-pagination>
     </el-card>
+
+    <!-- 添加用户的对话框 -->
+    <el-dialog title="添加用户" :visible.sync="addDialogVisible" width="50%" @close="addDialogClosed">
+      <!-- 内容主体区域 -->
+      <el-form :model="addForm" :rules="addFormRules" ref="addFormRef" label-width="70px">
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="addForm.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="addForm.password"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" prop="email">
+          <el-input v-model="addForm.email"></el-input>
+        </el-form-item>
+        <el-form-item label="手机" prop="phone">
+          <el-input v-model="addForm.phone"></el-input>
+        </el-form-item>
+      </el-form>
+      <!-- 底部区域 -->
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="addDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addUser">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -64,12 +89,52 @@
 import moment from "moment"; //导入文件
 export default {
   data() {
+    // 手机号验证
+    var checkPhone = (rule, value, callback) => {
+      const phoneReg = /^1[3|4|5|6|7|8][0-9]{9}$/;
+      if (!value) {
+        return callback(new Error("请输入手机号码"));
+      }
+      setTimeout(() => {
+        if (!Number.isInteger(+value)) {
+          callback(new Error("请输入数字值"));
+        } else {
+          if (phoneReg.test(value)) {
+            callback();
+          } else {
+            callback(new Error("电话号码格式不正确"));
+          }
+        }
+      }, 100);
+    };
     return {
       username: "",
       page: 1,
       rows: 2,
       userList: [],
-      total: 0
+      total: 0,
+      addDialogVisible: false,
+      addForm: {
+        username: "",
+        password: "",
+        email: "",
+        phone: ""
+      },
+      addFormRules: {
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" },
+          { min: 3, max: 10, message: "长度在 3 到 10 个字符", trigger: "blur" }
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" },
+          { min: 6, max: 15, message: "长度在 6 到 15 个字符", trigger: "blur" }
+        ],
+        email: [
+          { required: true, message: "请输入邮箱地址", trigger: "blur" },
+          { type: "email", message: "请输入正确的邮箱地址", trigger: "blur" }
+        ],
+        phone: [{ required: true, validator: checkPhone, trigger: "blur" }]
+      }
     };
   },
   created() {
@@ -111,6 +176,37 @@ export default {
     handleCurrentChange(newPage) {
       this.page = newPage;
       this.getUserList();
+    },
+    //监听switch开关状态的改变
+    userStateChanged(userinfo) {
+      this.$http({
+        method: "PUT",
+        url:
+          "users/status?userId=" + userinfo.id + "&enabled=" + userinfo.enabled
+      })
+        .then(resp => {
+          if (resp.data.code !== 200) {
+            userinfo.enabled = !userinfo.enabled;
+            return this.$message.error("更新用户状态失败！");
+          }
+          return this.$message.success("更新用户状态成功！");
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    searchUserList() {
+      this.page = 1;
+      this.getUserList();
+    },
+    //监听添加用户对话框的关闭事件
+    addDialogClosed() {
+      this.$refs.addFormRef.resetFields()
+    },
+    addUser() {
+      this.$refs.addFormRef.validate(valid => {
+        console.log(valid)
+      })
     }
   }
 };
