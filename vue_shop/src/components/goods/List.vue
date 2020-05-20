8ebe3dc79b4a0a1a7cb9fe11d704bed5b49ba9ef
hzl1998 <1445419939@qq.com>
2020-05-20 10:35:27 +0800

    ...

--- vue_shop/src/components/goods/List.vue
+++ vue_shop/src/components/goods/List.vue
@@ -7,11 +7,13 @@
     </el-breadcrumb>
 
     <el-card>
-      <el-row>
+      <el-row :gutter="20">
         <el-col :span="8">
           <el-input placeholder="请输入内容" prefix-icon="el-icon-search"></el-input>
         </el-col>
-        <el-col :span="4"></el-col>
+        <el-col :span="4">
+          <el-button type="primary">添加商品</el-button>
+        </el-col>
       </el-row>
     </el-card>
   </div>
