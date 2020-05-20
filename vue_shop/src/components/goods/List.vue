2bb4289743af7d90cb7388375485a746ec809dfc
hzl1998 <1445419939@qq.com>
2020-05-20 11:45:11 +0800

    商品列表

--- vue_shop/src/components/goods/List.vue
+++ vue_shop/src/components/goods/List.vue
@@ -15,6 +15,34 @@
           <el-button type="primary">添加商品</el-button>
         </el-col>
       </el-row>
+
+      <el-table :data="goodList" border stripe>
+        <el-table-column type="index" label="#"></el-table-column>
+        <el-table-column label="商品名称" prop="goods_name"></el-table-column>
+        <el-table-column label="商品价格(元)" prop="goods_price" width="95px"></el-table-column>
+        <el-table-column label="商品重量" prop="goods_weight" width="70px"></el-table-column>
+        <el-table-column label="创建时间" prop="add_time" width="140px">
+          <template slot-scope="scope">
+            {{scope.row.add_time | dateFormat}}
+          </template>
+        </el-table-column>
+        <el-table-column label="操作" width="130px">
+          <template slot-scope="scope">
+            <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
+            <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <el-pagination
+        background
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="page"
+        :page-sizes="[10, 20, 30, 40]"
+        :page-size="rows"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+      ></el-pagination>
     </el-card>
   </div>
 </template>
@@ -22,7 +50,44 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      page:1,
+      rows:10,
+      goodList:[],
+      total:0
+    };
+  },
+  created(){
+    this.getGoodsList()
+  },
+  methods:{
+    getGoodsList() {
+      this.$http({
+        method: "GET",
+        url: "goods" + "?page=" + this.page + "&rows=" + this.rows
+      })
+        .then(resp => {
+          if (resp.data.code !== 200) {
+            return this.$message.error("获取商品列表失败！");
+          }
+          console.log(resp.data.data)
+          this.goodList = resp.data.data.resultList
+          this.total = resp.data.data.totalResult
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    //监听pagesize改变的事件
+    handleSizeChange(nowSize) {
+      this.rows = nowSize;
+      this.getGoodsList();
+    },
+    //监听页码值改变的事件
+    handleCurrentChange(newPage) {
+      this.page = newPage;
+      this.getGoodsList();
+    },
   }
 };
 </script>
--- vue_shop/src/main.js
+++ vue_shop/src/main.js
@@ -11,6 +11,7 @@ import TreeTable from 'vue-table-with-tree-grid'
 
 
 import axios from 'axios'
+import { retry } from 'async'
 //配置请求的根路径
 axios.defaults.baseURL = 'http://localhost:8081/shop/'
 axios.interceptors.request.use(config => {
@@ -24,6 +25,19 @@ Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 Vue.component('tree-table',TreeTable)
+
+Vue.filter('dateFormat', function(originVal){
+  const dt = new Date(originVal * 1000)
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2,'0')
+  const d = (dt.getDate()+'').padStart(2,'0')
+  
+  const hh = (dt.getHours()+'').padStart(2,'0')
+  const mm = (dt.getMinutes()+'').padStart(2,'0')
+  const ss = (dt.getSeconds()+'').padStart(2,'0')
+
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 
 new Vue({
   router,
