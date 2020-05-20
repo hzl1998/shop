31a4196012cde7b99e687eeb41b11f25f93d6e78
hzl1998 <1445419939@qq.com>
2020-05-20 09:50:45 +0800

    ...

---
+++ vue_shop/src/components/goods/List.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品列表</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <el-card>
+      <el-row>
+        <el-col :span="8">
+          <el-input placeholder="请输入内容" prefix-icon="el-icon-search"></el-input>
+        </el-col>
+        <el-col :span="4"></el-col>
+      </el-row>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="less" scoped>
+</style>
