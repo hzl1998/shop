eca3df901cecd76da2cf8005a7ec4a4c62a95e43
hzl1998 <1445419939@qq.com>
2020-05-20 10:00:53 +0800

    cvbcvb

--- vue_shop_server/src/main/java/com/hzl/controller/CategoryController.java
+++ vue_shop_server/src/main/java/com/hzl/controller/CategoryController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@RestController("/goods")
 public class CategoryController {
     @Autowired
     CategoryService categoryService;
