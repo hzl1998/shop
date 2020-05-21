3e56257a985f7e4cb11f446f49d31dc68f4c97c4
hzl1998 <1445419939@qq.com>
2020-05-21 11:04:36 +0800

    ...

--- vue_shop_server/src/main/java/com/hzl/controller/FileController.java
+++ vue_shop_server/src/main/java/com/hzl/controller/FileController.java
@@ -8,12 +8,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@Controller
+@RestController
 public class FileController {
     /**
      *
