3a01dd483cf22622dfa3fe1f54291c4b1fcd31a3
hzl1998 <1445419939@qq.com>
2020-05-21 14:13:39 +0800

    123123

---
+++ vue_shop_server/src/main/java/com/hzl/configurer/MyWebAppConfigurer.java
@@ -0,0 +1,16 @@
+package com.hzl.configurer;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MyWebAppConfigurer implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**").
+                addResourceLocations("file:/" + "D://images/");
+    }
+}
