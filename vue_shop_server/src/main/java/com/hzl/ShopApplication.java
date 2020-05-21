f08b760f3e918393dea382e5452c2f5dc3fa8480
hzl1998 <1445419939@qq.com>
2020-05-21 12:13:59 +0800

    cvbxcv

--- vue_shop_server/src/main/java/com/hzl/ShopApplication.java
+++ vue_shop_server/src/main/java/com/hzl/ShopApplication.java
@@ -1,9 +1,12 @@
 package com.hzl;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 import javax.servlet.MultipartConfigElement;
 import java.io.File;
