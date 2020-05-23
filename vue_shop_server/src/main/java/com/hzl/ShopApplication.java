95d283bfa2487943f9a3bd80515a5eded997d77b
hzl1998 <1445419939@qq.com>
2020-05-23 13:00:26 +0800

    打包配置，已注释。。。

--- vue_shop_server/src/main/java/com/hzl/ShopApplication.java
+++ vue_shop_server/src/main/java/com/hzl/ShopApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
@@ -12,9 +14,15 @@ import javax.servlet.MultipartConfigElement;
 import java.io.File;
 
 @SpringBootApplication
-public class ShopApplication {
+public class ShopApplication /*extends SpringBootServletInitializer8*/ {
 
     public static void main(String[] args) {
         SpringApplication.run(ShopApplication.class, args);
     }
+
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        // 注意这里要指向原先用main方法执行的Application启动类
+//        return builder.sources(ShopApplication.class);
+//    }
 }
